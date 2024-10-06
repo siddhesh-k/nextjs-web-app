ff30cf9337b23e30dd2b5059a814c1614c63de3c
siddhesh-k <sskhadapakr22@gmail.com>
2024-10-06 15:17:09 +0530

    path changed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import SideNav from "../ui/dashboard/sidenav";
+import SideNav from "@/app/ui/dashboard/sidenav";
 
 export const experimental_ppr = true;
 export default function Layout({children}:{children:React.ReactNode}){
